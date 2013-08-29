79ffaa20311e6bc684b371b12865a6361a790375
Caro22 <carosc22@gmail.com>
2013-08-28 20:23:12 -0500

    un ojo a inner_find; avance hasta remove

--- AED/main.cpp
+++ AED/main.cpp
@@ -7,13 +7,12 @@ void showList(list<T>& A)
 {
     sizet A_size=A.size();
     for(size_t i=0; i<A_size; i++)
-        cout<<A[i]<<endl;
+        cout<<A[i]<<"\t";
+    cout<<"size: "<<A_size<<endl;
 }
 
 int main()
 {
-
-
     list<int> A;
     int a=2, b=3, c=4;
     A.push_back(a);
@@ -24,6 +23,5 @@ int main()
     showList(A);
     A.remove(a);
     showList(A);
-    cout<<"size: "<<A.size()<<endl;
 }
 
